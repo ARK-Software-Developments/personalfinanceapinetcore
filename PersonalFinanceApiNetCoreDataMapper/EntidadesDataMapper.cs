aa9f404dc942fd000d2682d6c41fe382f1af5bf5
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-22 17:39:21 -0300

    Nueva capa DataMapper

---
+++ PersonalFinanceApiNetCoreDataMapper/EntidadesDataMapper.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonalFinanceApiNetCoreDataMapper
+{
+    /// <summary>
+    /// Clase EntidadesDataMapper.
+    /// </summary>
+    public class EntidadesDataMapper
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntidadesDataMapper"/> class.
+        /// </summary>
+        public EntidadesDataMapper()
+        {
+        }
+
+        public static void GetAll()
+        {
+            var mysql = new MySQLConnectionDM();
+
+            var dr = mysql.GetDataReader("spEntitiesGetAll");
+        }
+    }
+}
---
+++ PersonalFinanceApiNetCoreDataMapper/MySQLConnectionDM.cs
@@ -0,0 +1,46 @@
+﻿namespace PersonalFinanceApiNetCoreDataMapper
+{
+    using System.Data;
+    using System.Data.Common;
+    using MySql.Data.MySqlClient;
+
+    /// <summary>
+    /// Clase de conexion a la bd.
+    /// </summary>
+    public class MySQLConnectionDM
+    {
+        public const string ConnectionString = "Server=localhost;Database=personalfinance;Uid=desarrollo;Pwd=PersonalFinance2025";
+
+        public MySqlConnection Connection { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MySQLConnectionDM"/> class.
+        /// </summary>
+        public MySQLConnectionDM()
+        {
+            this.Iniciar();
+        }
+
+        private void Iniciar()
+        {
+            this.Connection = new MySqlConnection(ConnectionString);
+            this.Connection.Open();
+        }
+
+        public DbDataReader GetDataReader(string spCommandName)
+        {
+            MySqlCommand cmd = new (spCommandName, this.Connection)
+            {
+                CommandType = CommandType.StoredProcedure,
+            };
+
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    return reader;
+                }
+            }
+        }
+    }
+}
