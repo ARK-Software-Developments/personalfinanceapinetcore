3ae4ae9749a431552079828803a903af7aaa16e4
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-22 17:39:34 -0300

    Nueva capa logica de negocio

---
+++ PersonalFinanceApiNetCoreBL/EntidadesBL.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalFinanceApiNetCoreBL
+{
+    using PersonalFinanceApiNetCoreModel;
+
+    /// <summary>
+    /// Clase EntidadesBL.
+    /// </summary>
+    public class EntidadesBL
+    {
+        public List<Entidad> GetAll()
+        {
+            return new List<Entidad>();
+        }
+    }
+}
