6ef75315037744e99322b76cfb1f31c1b2556a68
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-22 16:26:27 -0300

    ultima actualizacion

---
+++ PersonalFinanceApiNetCore/Models/Entidad.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalFinanceApiNetCore.Models
+{
+    public class Entidad
+    {   
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+    }
+}
