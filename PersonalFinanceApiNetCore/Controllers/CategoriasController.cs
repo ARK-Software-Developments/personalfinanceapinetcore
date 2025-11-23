4db67739a31cc2f00d11605d2ae2c84fe3bceed8
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-23 17:24:51 -0300

    Se agrega nuevo servicio para Categorias getall, getid, create y update

---
+++ PersonalFinanceApiNetCore/Controllers/CategoriasController.cs
@@ -0,0 +1,128 @@
+﻿
+
+namespace PersonalFinanceApiNetCore.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using PersonalFinanceApiNetCoreBL;
+    using PersonalFinanceApiNetCoreModel;
+
+    /// <summary>
+    /// CategoriasController.
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/categories")]
+    [UserSystemTextJsonAttribute]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ILogger<CategoriasController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CategoriasController"/> class.
+        /// </summary>
+        /// <param name="logger">ILogger.</param>
+        public CategoriasController(ILogger<CategoriasController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// GetAll.
+        /// </summary>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpGet("getall")]
+        public GeneralResponse GetAll()
+        {
+            List<Categoria> entidades = new CategoriasBL().GetAll();
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "get",
+                    Operacion = "getall",
+                    Recurso = string.Empty,
+                },
+                Errores = null,
+                Data = entidades,
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// GetId.
+        /// </summary>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpGet("get/{id}")]
+        public GeneralResponse GetId(int id)
+        {
+            List<Categoria> entidades = new CategoriasBL().GetId(id);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "get/{id}",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// AddEntity.
+        /// </summary>
+        /// <param name="parametros">Parametro lista.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpPut("create")]
+        public GeneralResponse AddEntity([FromBody] List<Parametro> parametros)
+        {
+           long entidades = new CategoriasBL().AddUpdateEntity("create", parametros);
+
+           var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "create",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+           return response;
+        }
+
+        /// <summary>
+        /// AddEntity.
+        /// </summary>
+        /// <param name="parametros">Parametro lista.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpPut("update")]
+        public GeneralResponse UpdateEntity([FromBody] List<Parametro> parametros)
+        {
+            long entidades = new CategoriasBL().AddUpdateEntity("update", parametros);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "update",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+            return response;
+        }
+    }
+}
