81ecb893709186eeed9e72bfd6d370e42241cd9a
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-25 17:26:49 -0300

    Capas model, datamapper, bl y controller Transacciones

---
+++ PersonalFinanceApiNetCore/Controllers/TransaccionesController.cs
@@ -0,0 +1,121 @@
+﻿namespace PersonalFinanceApiNetCore.Controllers
+{
+#pragma warning disable CS8625
+#pragma warning disable SA1309
+#pragma warning disable SA1009
+
+    using Microsoft.AspNetCore.Mvc;
+    using PersonalFinanceApiNetCoreBL;
+    using PersonalFinanceApiNetCoreModel;
+
+    /// <summary>
+    /// TransaccionesController.
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/transactions")]
+    [UserSystemTextJsonAttribute]
+    public class TransaccionesController(ILogger<TransaccionesController> logger) : ControllerBase
+    {
+        private readonly ILogger<TransaccionesController> _logger = logger;
+
+        /// <summary>
+        /// GetAll.
+        /// </summary>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpGet("getall")]
+        public GeneralResponse GetAll()
+        {
+            List<Transaccion> entidades = new TransaccionesBL().GetAll();
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "get",
+                    Operacion = "getall",
+                    Recurso = string.Empty,
+                },
+                Errores = null,
+                Data = entidades,
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// GetId.
+        /// </summary>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpGet("get/{id}")]
+        public GeneralResponse GetId(int id)
+        {
+            List<Transaccion> entidades = new TransaccionesBL().GetId(id);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "get/{id}",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// AddEntity.
+        /// </summary>
+        /// <param name="parametros">Parametro lista.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpPut("create")]
+        public GeneralResponse AddEntity([FromBody] List<Parametro> parametros)
+        {
+           long entidades = new TransaccionesBL().AddUpdateEntity("create", parametros);
+
+           var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "create",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+           return response;
+        }
+
+        /// <summary>
+        /// AddEntity.
+        /// </summary>
+        /// <param name="parametros">Parametro lista.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpPut("update")]
+        public GeneralResponse UpdateEntity([FromBody] List<Parametro> parametros)
+        {
+            long entidades = new TransaccionesBL().AddUpdateEntity("update", parametros);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "update",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+            return response;
+        }
+    }
+}
