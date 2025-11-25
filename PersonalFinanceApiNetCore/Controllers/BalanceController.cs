8bf472e2c7551934bdba5ac76c9483c6372b25ff
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-25 12:03:37 -0300

    Capas model, datamapper, bl y controller Balance

---
+++ PersonalFinanceApiNetCore/Controllers/BalanceController.cs
@@ -0,0 +1,122 @@
+﻿namespace PersonalFinanceApiNetCore.Controllers
+{
+#pragma warning disable CS8625
+#pragma warning disable SA1309
+#pragma warning disable SA1009
+
+    using Microsoft.AspNetCore.Mvc;
+    using PersonalFinanceApiNetCoreBL;
+    using PersonalFinanceApiNetCoreModel;
+
+    /// <summary>
+    /// BalanceController.
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/balance")]
+    [UserSystemTextJsonAttribute]
+    public class BalanceController(ILogger<BalanceController> logger) : ControllerBase
+    {
+        private readonly ILogger<BalanceController> _logger = logger;
+
+        /// <summary>
+        /// GetAll.
+        /// </summary>
+        /// <param name="ano">Año de ejercicio.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpGet("getall/{ano}")]
+        public GeneralResponse GetAll(int ano)
+        {
+            List<Balance> entidades = new BalanceBL().GetAllByYear(ano);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "get",
+                    Operacion = "getallbyyear",
+                    Recurso = string.Empty,
+                },
+                Errores = null,
+                Data = entidades,
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// GetId.
+        /// </summary>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpGet("get/{id}")]
+        public GeneralResponse GetId(int id)
+        {
+            List<Balance> entidades = new BalanceBL().GetId(id);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "get/{id}",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// AddEntity.
+        /// </summary>
+        /// <param name="parametros">Parametro lista.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpPut("create")]
+        public GeneralResponse AddEntity([FromBody] List<Parametro> parametros)
+        {
+           long entidades = new BalanceBL().AddUpdateEntity("create", parametros);
+
+           var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "create",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+           return response;
+        }
+
+        /// <summary>
+        /// AddEntity.
+        /// </summary>
+        /// <param name="parametros">Parametro lista.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpPut("update")]
+        public GeneralResponse UpdateEntity([FromBody] List<Parametro> parametros)
+        {
+            long entidades = new BalanceBL().AddUpdateEntity("update", parametros);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "post",
+                    Operacion = "update",
+                    Recurso = string.Empty,
+                },
+                Data = entidades,
+            };
+
+            return response;
+        }
+    }
+}
---
+++ PersonalFinanceApiNetCoreBL/BalanceBL.cs
@@ -0,0 +1,69 @@
+﻿namespace PersonalFinanceApiNetCoreBL
+{
+    using PersonalFinanceApiNetCoreDataMapper;
+    using PersonalFinanceApiNetCoreModel;
+    using System;
+
+    /// <summary>
+    /// Clase BalanceBL.
+    /// </summary>
+    public class BalanceBL
+    {
+        private BalanceDataMapper mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BalanceBL"/> class.
+        /// </summary>
+        public BalanceBL()
+        {
+            this.mapper = new ();
+        }
+
+        /// <summary>
+        /// Método para obtener todos los registros de la categorias.
+        /// </summary>
+        /// <returns>Lista de categorias.</returns>
+        public List<Balance> GetAll()
+        {
+            return this.mapper.GetAll<Balance>();
+        }
+
+        /// <summary>
+        /// Método para obtener un solo registro.
+        /// </summary>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>Lista de entida.</returns>
+        public List<Balance> GetId(int id)
+        {
+            return this.mapper.GetId<Balance>(id);
+        }
+
+        /// <summary>
+        /// Método para obtener un solo registro.
+        /// </summary>
+        /// <param name="operacion">Operacion Create/Update.</param>
+        /// <param name="parametros">Informacion a agregar/actualizar.</param>
+        /// <returns>Lista de entida.</returns>
+        public long AddUpdateEntity(string operacion, List<Parametro> parametros)
+        {
+            if (operacion == "create")
+            {
+                return this.mapper.AddEntity(parametros);
+            }
+            else
+            {
+                return this.mapper.UpdateEntity(parametros);
+            }
+        }
+
+        /// <summary>
+        /// Método para obtener todos los registros de la categorias.
+        /// </summary>
+        /// <param name="ano">Año.</param>
+        /// <returns>Lista de categorias.</returns>
+        public List<Balance> GetAllByYear(int ano)
+        {
+            return this.mapper.GetAll<Balance>(ano);
+        }
+    }
+}
