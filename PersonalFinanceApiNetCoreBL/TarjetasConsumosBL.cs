8b05f3300fd4f71d2e23bc53f316f5c7e9ed66fa
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-26 13:55:10 -0300

    Capas model, datamapper, bl y controller de creditcardspending

---
+++ PersonalFinanceApiNetCoreBL/TarjetasConsumosBL.cs
@@ -0,0 +1,68 @@
+﻿namespace PersonalFinanceApiNetCoreBL
+{
+    using PersonalFinanceApiNetCoreDataMapper;
+    using PersonalFinanceApiNetCoreModel;
+
+    /// <summary>
+    /// Clase TarjetasConsumosBL.
+    /// </summary>
+    public class TarjetasConsumosBL
+    {
+        private TarjetasConsumosDataMapper mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TarjetasConsumosBL"/> class.
+        /// </summary>
+        public TarjetasConsumosBL()
+        {
+            this.mapper = new ();
+        }
+
+        /// <summary>
+        /// Método para obtener todos los registros de la categorias.
+        /// </summary>
+        /// <returns>Lista de categorias.</returns>
+        public List<TarjetaConsumo> GetAll()
+        {
+            return this.mapper.GetAll<TarjetaConsumo>();
+        }
+
+        /// <summary>
+        /// Método para obtener un solo registro.
+        /// </summary>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>Lista de entida.</returns>
+        public List<TarjetaConsumo> GetId(int id)
+        {
+            return this.mapper.GetId<TarjetaConsumo>(id);
+        }
+
+        /// <summary>
+        /// Método para obtener un solo registro.
+        /// </summary>
+        /// <param name="operacion">Operacion Create/Update.</param>
+        /// <param name="parametros">Informacion a agregar/actualizar.</param>
+        /// <returns>Lista de entida.</returns>
+        public long AddUpdateEntity(string operacion, List<Parametro> parametros)
+        {
+            if (operacion == "create")
+            {
+                return this.mapper.AddEntity(parametros);
+            }
+            else
+            {
+                return this.mapper.UpdateEntity(parametros);
+            }
+        }
+
+        /// <summary>
+        /// Método para obtener todos los registros de la categorias.
+        /// </summary>
+        /// <param name="ano">Año.</param>
+        /// <returns>Lista de categorias.</returns>
+        public List<TarjetaConsumo> GetAllByYear(int ano)
+        {
+            return this.mapper.GetAll<TarjetaConsumo>(ano);
+        }
+    }
+}
---
+++ PersonalFinanceApiNetCoreDataMapper/TarjetasConsumosDataMapper.cs
@@ -0,0 +1,159 @@
+﻿namespace PersonalFinanceApiNetCoreDataMapper
+{
+#pragma warning disable CA1822
+#pragma warning disable CS8601
+#pragma warning disable CS8618
+#pragma warning disable SA1623
+
+    using MySql.Data.MySqlClient;
+    using PersonalFinanceApiNetCoreModel;
+    using PersonalFinanceApiNetCoreModel.Interfaces;
+
+    /// <summary>
+    /// Clase TarjetasConsumosDataMapper.
+    /// </summary>
+    public class TarjetasConsumosDataMapper : IDataMapper
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TarjetasConsumosDataMapper"/> class.
+        /// </summary>
+        public TarjetasConsumosDataMapper()
+        {
+        }
+
+        /// <summary>
+        /// Metodo para obtener todos los registros.
+        /// </summary>
+        /// <typeparam name="T">Lista del tipo.</typeparam>
+        /// <returns>Lista de categorias.</returns>
+        public List<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Metodo para obtener todos los registros.
+        /// </summary>
+        /// <typeparam name="T">Lista del tipo.</typeparam>
+        /// <param name="ano">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public List<T> GetAll<T>(int ano)
+        {
+            var lstEntidades = new List<TarjetaConsumo>();
+
+            var mysql = new MySQLConnectionDM();
+
+            List<Parametro> parametros =
+            [
+                new ()
+                {
+                    Nombre = "pYear",
+                    Valor = ano,
+                },
+            ];
+
+            var mySqlDataReader = mysql.GetDataReader("spCreditCardSpendingtGetAll", parametros);
+
+            while (mySqlDataReader.Read())
+            {
+                lstEntidades.Add(this.MapperData(mySqlDataReader));
+            }
+
+            return (List<T>)Convert.ChangeType(lstEntidades, typeof(List<TarjetaConsumo>));
+        }
+
+        /// <summary>
+        /// Metodo para obtener un registro.
+        /// </summary>
+        /// <typeparam name="T">Lista del tipo.</typeparam>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public List<T> GetId<T>(int id)
+        {
+            var lstEntidades = new List<TarjetaConsumo>();
+
+            var mysql = new MySQLConnectionDM();
+
+            List<Parametro> parametros =
+            [
+                new ()
+                {
+                    Nombre = "pid",
+                    Valor = id,
+                },
+            ];
+
+            var mySqlDataReader = mysql.GetDataReader("spCreditCardSpendingtGetId", parametros);
+
+            while (mySqlDataReader.Read())
+            {
+                lstEntidades.Add(this.MapperData(mySqlDataReader));
+            }
+
+            return (List<T>)Convert.ChangeType(lstEntidades, typeof(List<TarjetaConsumo>));
+        }
+
+        /// <summary>
+        /// Metodo para agregar un registro nuevo.
+        /// </summary>
+        /// <param name="parametros">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public long AddEntity(List<Parametro> parametros)
+        {
+            return new MySQLConnectionDM().Add("spCreditCardSpendingtAdd", parametros);
+        }
+
+        /// <summary>
+        /// Metodo para actualizar un registro.
+        /// </summary>
+        /// <param name="parametros">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public long UpdateEntity(List<Parametro> parametros)
+        {
+            return new MySQLConnectionDM().Update("spCreditCardSpendingtUpdate", parametros);
+        }
+
+        /// <summary>
+        /// Mapeo de registro.
+        /// </summary>
+        /// <param name="mySqlDataReader">MySqlDataReader.</param>
+        /// <returns>Entidad respectiva.</returns>
+        private TarjetaConsumo MapperData(MySqlDataReader mySqlDataReader)
+        {
+            TarjetaConsumo entidad = new ()
+            {
+                Id = Convert.ToInt32(mySqlDataReader["id"]),
+                Enero = (decimal)mySqlDataReader["january"],
+                Febrero = (decimal)mySqlDataReader["february"],
+                Marzo = (decimal)mySqlDataReader["march"],
+                Abril = (decimal)mySqlDataReader["april"],
+                Mayo = (decimal)mySqlDataReader["may"],
+                Junio = (decimal)mySqlDataReader["june"],
+                Julio = (decimal)mySqlDataReader["july"],
+                Agosto = (decimal)mySqlDataReader["august"],
+                Septiembre = (decimal)mySqlDataReader["september"],
+                Octubre = (decimal)mySqlDataReader["october"],
+                Noviembre = (decimal)mySqlDataReader["november"],
+                Diciembre = (decimal)mySqlDataReader["december"],
+                Ano = (int)mySqlDataReader["year"],
+                Detalle = mySqlDataReader["details"].ToString(),
+                Cuotas = (int)mySqlDataReader["numberinstallments"],
+                Verificado = (bool)mySqlDataReader["verified"],
+                Pagado = (bool)mySqlDataReader["paid"],
+                Tarjeta = new Tarjeta()
+                {
+                    Id = (int)mySqlDataReader["cardsid"],
+                    Nombre = mySqlDataReader["cardname"].ToString(),
+                },
+                Transaccion = new Transaccion()
+                {
+                    Id = (int)mySqlDataReader["transactioncodeid"],
+                    EntidadAsociada = mySqlDataReader["associatedentity"].ToString(),
+                },
+                EntidadCompra = mySqlDataReader["purchasingentity"].ToString(),
+            };
+
+            return entidad;
+        }
+    }
+}
