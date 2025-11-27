0b820db4d00f41277ab84a76c9f290a98d09b36f
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-27 15:03:38 -0300

    Capas model, datamapper, bl y controller de income

---
+++ PersonalFinanceApiNetCoreBL/IngresosBL.cs
@@ -0,0 +1,68 @@
+﻿namespace PersonalFinanceApiNetCoreBL
+{
+    using PersonalFinanceApiNetCoreDataMapper;
+    using PersonalFinanceApiNetCoreModel;
+
+    /// <summary>
+    /// Clase IngresosBL.
+    /// </summary>
+    public class IngresosBL
+    {
+        private IngresoDataMapper mapper;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IngresosBL"/> class.
+        /// </summary>
+        public IngresosBL()
+        {
+            this.mapper = new ();
+        }
+
+        /// <summary>
+        /// Método para obtener todos los registros de la categorias.
+        /// </summary>
+        /// <returns>Lista de categorias.</returns>
+        public List<Ingreso> GetAll()
+        {
+            return this.mapper.GetAll<Ingreso>();
+        }
+
+        /// <summary>
+        /// Método para obtener un solo registro.
+        /// </summary>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>Lista de entida.</returns>
+        public List<Ingreso> GetId(int id)
+        {
+            return this.mapper.GetId<Ingreso>(id);
+        }
+
+        /// <summary>
+        /// Método para obtener un solo registro.
+        /// </summary>
+        /// <param name="operacion">Operacion Create/Update.</param>
+        /// <param name="parametros">Informacion a agregar/actualizar.</param>
+        /// <returns>Lista de entida.</returns>
+        public long AddUpdateEntity(string operacion, List<Parametro> parametros)
+        {
+            if (operacion == "create")
+            {
+                return this.mapper.AddEntity(parametros);
+            }
+            else
+            {
+                return this.mapper.UpdateEntity(parametros);
+            }
+        }
+
+        /// <summary>
+        /// Método para obtener todos los registros de la categorias.
+        /// </summary>
+        /// <param name="ano">Año.</param>
+        /// <returns>Lista de categorias.</returns>
+        public List<Ingreso> GetAllByYear(int ano)
+        {
+            return this.mapper.GetAll<Ingreso>(ano);
+        }
+    }
+}
---
+++ PersonalFinanceApiNetCoreDataMapper/IngresoDataMapper.cs
@@ -0,0 +1,148 @@
+﻿namespace PersonalFinanceApiNetCoreDataMapper
+{
+#nullable disable
+#pragma warning disable CA1822
+
+    using MySql.Data.MySqlClient;
+    using PersonalFinanceApiNetCoreModel;
+    using PersonalFinanceApiNetCoreModel.Interfaces;
+
+    /// <summary>
+    /// Clase IngresoDataMapper.
+    /// </summary>
+    public class IngresoDataMapper : IDataMapper
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IngresoDataMapper"/> class.
+        /// </summary>
+        public IngresoDataMapper()
+        {
+        }
+
+        /// <summary>
+        /// Metodo para obtener todos los registros.
+        /// </summary>
+        /// <typeparam name="T">Lista del tipo.</typeparam>
+        /// <returns>Lista de categorias.</returns>
+        public List<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Metodo para obtener todos los registros.
+        /// </summary>
+        /// <typeparam name="T">Lista del tipo.</typeparam>
+        /// <param name="ano">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public List<T> GetAll<T>(int ano)
+        {
+            var lstEntidades = new List<Ingreso>();
+
+            var mysql = new MySQLConnectionDM();
+
+            List<Parametro> parametros =
+            [
+                new ()
+                {
+                    Nombre = "pYear",
+                    Valor = ano,
+                },
+            ];
+
+            var mySqlDataReader = mysql.GetDataReader("spIncomeGetAll", parametros);
+
+            while (mySqlDataReader.Read())
+            {
+                lstEntidades.Add(this.MapperData(mySqlDataReader));
+            }
+
+            return (List<T>)Convert.ChangeType(lstEntidades, typeof(List<Ingreso>));
+        }
+
+        /// <summary>
+        /// Metodo para obtener un registro.
+        /// </summary>
+        /// <typeparam name="T">Lista del tipo.</typeparam>
+        /// <param name="id">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public List<T> GetId<T>(int id)
+        {
+            var lstEntidades = new List<Ingreso>();
+
+            var mysql = new MySQLConnectionDM();
+
+            List<Parametro> parametros =
+            [
+                new ()
+                {
+                    Nombre = "pid",
+                    Valor = id,
+                },
+            ];
+
+            var mySqlDataReader = mysql.GetDataReader("spIncomeGetId", parametros);
+
+            while (mySqlDataReader.Read())
+            {
+                lstEntidades.Add(this.MapperData(mySqlDataReader));
+            }
+
+            return (List<T>)Convert.ChangeType(lstEntidades, typeof(List<Ingreso>));
+        }
+
+        /// <summary>
+        /// Metodo para agregar un registro nuevo.
+        /// </summary>
+        /// <param name="parametros">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public long AddEntity(List<Parametro> parametros)
+        {
+            return new MySQLConnectionDM().Add("spIncomeAdd", parametros);
+        }
+
+        /// <summary>
+        /// Metodo para actualizar un registro.
+        /// </summary>
+        /// <param name="parametros">Id del registro.</param>
+        /// <returns>Lista de categorias.</returns>
+        public long UpdateEntity(List<Parametro> parametros)
+        {
+            return new MySQLConnectionDM().Update("spIncomeUpdate", parametros);
+        }
+
+        /// <summary>
+        /// Mapeo de registro.
+        /// </summary>
+        /// <param name="mySqlDataReader">MySqlDataReader.</param>
+        /// <returns>Entidad respectiva.</returns>
+        private Ingreso MapperData(MySqlDataReader mySqlDataReader)
+        {
+            Ingreso entidad = new ()
+            {
+                Id = Convert.ToInt32(mySqlDataReader["id"]),
+                Enero = (decimal)mySqlDataReader["january"],
+                Febrero = (decimal)mySqlDataReader["february"],
+                Marzo = (decimal)mySqlDataReader["march"],
+                Abril = (decimal)mySqlDataReader["april"],
+                Mayo = (decimal)mySqlDataReader["may"],
+                Junio = (decimal)mySqlDataReader["june"],
+                Julio = (decimal)mySqlDataReader["july"],
+                Agosto = (decimal)mySqlDataReader["august"],
+                Septiembre = (decimal)mySqlDataReader["september"],
+                Octubre = (decimal)mySqlDataReader["october"],
+                Noviembre = (decimal)mySqlDataReader["november"],
+                Diciembre = (decimal)mySqlDataReader["december"],
+                Ano = (int)mySqlDataReader["year"],
+                Detalle = new IngresoDetalle()
+                {
+                    Id = (int)mySqlDataReader["incomedetailsid"],
+                    Codigo = (int)mySqlDataReader["code"],
+                    Detalle = mySqlDataReader["detail"].ToString(),
+                },
+            };
+
+            return entidad;
+        }
+    }
+}
