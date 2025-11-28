57fed38fcf8ddb29fc316edbd5fad4235e8441d6
Andres Kamycki <andres.kamycki@gmail.com>
2025-11-28 16:29:36 -0300

    Se agrega AddCors

--- PersonalFinanceApiNetCore/Program.cs
+++ PersonalFinanceApiNetCore/Program.cs
@@ -8,6 +8,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: "AllowSpecificOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000") // Specify the allowed origin
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .AllowCredentials(); // If you need to send cookies or authentication headers
+        });
+});
+
 var config = builder.Configuration.GetConnectionString("Default");
 
 var app = builder.Build();
@@ -18,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseHttpsRedirection();
 
