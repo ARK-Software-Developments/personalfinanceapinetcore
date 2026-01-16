941f4ad24d80fbc8d80b3d44e24e768d285dc855
Andres Kamycki <andres.kamycki@gmail.com>
2026-01-15 23:28:43 -0300

    Agregar al Inicio La dif entre Prespuesto y Pagado + Listar Gastos pendientes de pago

---
+++ PersonalFinanceApiNetCoreModel/CalendarioVencimiento.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PersonalFinanceApiNetCoreModel
+{
+    /// <summary>
+    /// Clase Ingreso.
+    /// </summary>
+    public class CalendarioVencimiento : AbstractModelExternder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalendarioVencimiento"/> class.
+        /// </summary>
+        public CalendarioVencimiento()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets propiedad FechaVencimiento.
+        /// </summary>
+        [JsonPropertyOrder(2)]
+        public DateTime FechaVencimiento { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad TipoGastoId.
+        /// </summary>
+        [JsonPropertyOrder(3)]
+        public int TipoGastoId { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad Activo.
+        /// </summary>
+        [JsonPropertyOrder(4)]
+        public bool Activo { get; set; }
+    }
+}
