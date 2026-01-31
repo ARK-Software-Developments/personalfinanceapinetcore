aed73fce56feccbb6162c881fd300284fc2d59b0
Andres Kamycki <andres.kamycki@gmail.com>
2026-01-30 22:50:26 -0300

    Balance update

--- PersonalFinanceApiNetCore/Controllers/ProcesosController.cs
+++ PersonalFinanceApiNetCore/Controllers/ProcesosController.cs
@@ -5,7 +5,6 @@
 #pragma warning disable SA1009
 
     using Microsoft.AspNetCore.Mvc;
-    using PersonalFinanceApiNetCoreBL;
     using PersonalFinanceApiNetCoreBL.Procesos;
     using PersonalFinanceApiNetCoreModel;
 
@@ -56,6 +55,33 @@
         public GeneralResponse IniciarProcesoActualizacionGM([FromBody] List<Parametro> parametros)
         {
             new ProcesoBalanceBL().IniciarProcesoGM(parametros);
+
+            var response = new GeneralResponse()
+            {
+                Meta = new Meta()
+                {
+                    Metodo = "put",
+                    Operacion = "update",
+                    Recurso = string.Empty,
+                },
+                Data = new List<object>() { true },
+            };
+
+            return response;
+        }
+
+        /// <summary>
+        /// AddEntity.
+        /// </summary>
+        /// <param name="parametros">Parametro lista.</param>
+        /// <returns>GeneralResponse.</returns>
+        [Produces("application/json")]
+        [HttpPut("monthlybalance")]
+        public GeneralResponse MonthlyBalance([FromBody] List<Parametro> parametros)
+        {
+            new ProcesoBalanceBL().IniciarProcesoUpdateBalance(parametros);
+
+            new ProcesoBalanceBL().IniciarProcesoUpdateBalanceIngreso(parametros);
 
             var response = new GeneralResponse()
             {
